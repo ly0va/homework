2611df3df1afffa51684386699464589ed743136
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2020-10-16 19:28:46 +0300

    Save, load and recalculate

--- KNU/oop/lab1/spreadsheet.py
+++ KNU/oop/lab1/spreadsheet.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import re
+import pickle
 import gi
 
 gi.require_version('Gtk', '3.0')
@@ -17,7 +18,7 @@ class Spreadsheet(gtk.Window):
         self.parser = Parser(lexer)
         self.cols = cols
         self.rows = rows
-        self.formulas = [['0'] * cols for _ in range(rows)]
+        self.formulas = [[''] * cols for _ in range(rows)]
         self.init_widgets()
         self.add_table()
 
@@ -31,9 +32,69 @@ class Spreadsheet(gtk.Window):
         self.window.add(self.treeview)
         self.entry = gtk.Entry()
         vbox = gtk.VBox(homogeneous=False, spacing=8)
+        vbox.pack_start(self.create_toolbar(), False, False, 0)
         vbox.pack_start(self.entry, False, False, 0)
         vbox.pack_start(self.window, True, True, 0)
         self.add(vbox)
+
+    def create_toolbar(self):
+        toolbar = gtk.Toolbar()
+        load = gtk.ToolButton(stock_id=gtk.STOCK_OPEN)
+        save = gtk.ToolButton(stock_id=gtk.STOCK_SAVE)
+        quit = gtk.ToolButton(stock_id=gtk.STOCK_QUIT)
+        info = gtk.ToolButton(stock_id=gtk.STOCK_ABOUT)
+        toolbar.insert(load, 0)
+        toolbar.insert(save, 1)
+        toolbar.insert(quit, 2)
+        toolbar.insert(info, 3)
+        load.connect("clicked", lambda _: self.load())
+        save.connect("clicked", lambda _: self.save())
+        quit.connect("clicked", gtk.main_quit)
+        info.connect("clicked", lambda _: self.info(
+            "Lyova's Spreadsheet v0.1.0", 
+            "Lab 1. Ticket 20. Lev Potyomkin, K-27"
+        ))
+        return toolbar
+
+    def save(self):
+        try:
+            with open('spreadsheet.pkl', 'wb') as file:
+                pickle.dump(self.formulas, file)
+            self.info("File saved successfully")
+        except Exception:
+            self.error("Error saving file :(")
+
+    def load(self):
+        try:
+            with open('spreadsheet.pkl', 'rb') as file:
+                self.formulas = pickle.load(file)
+            self.recalculate()
+            self.info("File loaded successfully")
+        except Exception:
+            self.error("Error loading file :(")
+
+    def recalculate(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.parse(i, j, self.formulas[i][j])
+
+    def parse(self, row, col, text):
+        if text == '':
+            self.formulas[row][col] = ''
+            self.values[str(row)][col] = ''
+            return
+        old_formula = self.formulas[row][col]
+        self.formulas[row][col] = text
+        try:
+            parsed = self.parser.parse(self.expand(text))
+            self.values[str(row)][col] = str(parsed)
+        except RecursionError:
+            self.error("Cycle detected!")
+            self.formulas[row][col] = old_formula
+        except SyntaxError as error:
+            self.error(error.args[0])
+            self.formulas[row][col] = old_formula
+                
 
     def add_table(self):
         types = [str] * (self.cols + 1)
@@ -61,36 +122,27 @@ class Spreadsheet(gtk.Window):
 
     def update(self, cell, row, text):
         row, col = int(row), cell.column
-        old_formula = self.formulas[row][col]
-        self.formulas[row][col] = text
-        try:
-            parsed = self.parser.parse(self.expand(text))
-            self.values[str(row)][col] = str(parsed)
-        except RecursionError:
-            self.error("Cycle detected!")
-            self.formulas[row][col] = old_formula
-        except SyntaxError as error:
-            self.error(error.args[0])
-            self.formulas[row][col] = old_formula
+        self.parse(row, col, text)
+        self.recalculate()
 
     def expand(self, formula):
         def replacer(match):
             name = match[0]
             col, row = ord(name[0]) - ord('A'), int(name[1:])
-            return f'({self.formulas[row][col]})'
+            return f'({self.formulas[row][col] or "0"})'
 
         if not re.search(CELL_REGEXP, formula):
             return formula
         return self.expand(re.sub(CELL_REGEXP, replacer, formula))
 
-    def about(self):
+    def info(self, primary, secondary=''):
         dialog = gtk.MessageDialog(
             transient_for=self,
             message_type=gtk.MessageType.INFO,
             buttons=gtk.ButtonsType.OK,
-            text="Lyova's Spreadsheet v0.1.0",
+            text=primary,
         )
-        dialog.format_secondary_text("Lab 1. Ticket 20. Lev Potyomkin, K-27")
+        dialog.format_secondary_text(secondary)
         dialog.run()
         dialog.destroy()
 
