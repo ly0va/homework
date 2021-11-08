eb1bc504e0be90fec7d482a32837d5b7e2cc3694
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2021-11-08 04:49:46 +0200

    More tests and fixed

--- KNU/algebra/algebra.py
+++ KNU/algebra/algebra.py
@@ -3,7 +3,13 @@
 from bigint import BigInt
 
 def pollard_rho(number, iters=10):
+    assert number % 2 == 1
+
     number = BigInt(number)
+    sqrt = number.sqrt()
+    if sqrt ** 2 == number:
+        return sqrt
+
     x = BigInt(2)
     attempts = BigInt(1)
 
@@ -12,7 +18,6 @@ def pollard_rho(number, iters=10):
         attempts *= 2
         count = attempts
         while count > 0:
-            print(_, count)
             x = (x * x + 1) % number
             factor = BigInt.gcd(abs(x - y), number)
             if factor > 1 and factor != number:
@@ -23,6 +28,13 @@ def pollard_rho(number, iters=10):
 
 
 def factorize(number):
+    number = BigInt(number)
+    if number <= 3: return number
+
+    while number % 2 == 0:
+        yield BigInt(2)
+        number //= 2
+
     while True:
         factor = pollard_rho(number)
         if factor != 1:
@@ -37,29 +49,34 @@ def factorize(number):
 def legendre_symbol(a, p):
     assert p > 2
     # assert is_prime(p)
-    if a % p == 0: return 0
-    return pow(a, (p-1)//2, p)
 
+    a, p = BigInt(a), BigInt(p)
+    if a % p == 0: return 0
+    result = pow(a, (p-1)//2, p)
+    if result == p-1: result = -1
+    return result
 
 def jacobi_symbol(a, p):
     assert p > 1 and p % 2 == 1
 
+    a, p = BigInt(a), BigInt(p)
     result = 1
     for factor in factorize(p):
         result *= legendre_symbol(a, factor)
 
     return result
 
-
 def euler_phi(a):
-    result = a.copy()
-    for p in factorize(a):
-        result -= result / p
+    result = BigInt(a)
+    factors = sorted(set(factorize(a)))
+    print(factors)
+    for p in factors:
+        result -= result // p
     return result
 
 def mobius_mu(a):
     factors = []
-    for factor in factorize(a):
+    for factor in sorted(factorize(a)):
         if factors == [] or factors[-1] != factor:
             factors.append(factor)
         else:
@@ -121,7 +138,7 @@ def discrete_sqrt_cipolla(n, p):
     assert p > 2
     # assert is_prime(p)
 
-    a = BigInt(1)
+    n, p, a = BigInt(n), BigInt(p), BigInt(1)
     while legendre_symbol(a*a - n, p) != -1:
         a += 1
 
@@ -129,10 +146,10 @@ def discrete_sqrt_cipolla(n, p):
     power = (p + 1)//2
     u, v = a, BigInt(1)
     x, y = BigInt(1), BigInt(0)
-    
+
     while power > 0:
         if power % 2 == 1:
-            result = (x*u + y*v*k) % p, (x*v + y*u) % p
+            x, y = (x*u + y*v*k) % p, (x*v + y*u) % p
         u, v = (u*u + v*v*k) % p, 2*u*v % p
         power //= 2
 
--- KNU/algebra/test_algebra.py
+++ KNU/algebra/test_algebra.py
@@ -23,6 +23,29 @@ def test_discrete_log():
     assert pow(BigInt(18), x, 4049) == 103
 
 def test_discrete_sqrt():
-    # TODO fix modulo of negative number ?
-    # assert discrete_sqrt_cipolla(5, 11) == 4
-    pass
+    x = discrete_sqrt_cipolla(5, 11)
+    assert x*x % 11 == 5
+
+    x = discrete_sqrt_cipolla(23, 101)
+    assert x*x % 101 == 23
+
+def test_legendre():
+    assert legendre_symbol(12345, 331) == -1
+    assert legendre_symbol(35, 7) == 0
+    assert legendre_symbol(4, 7) == 1
+
+def test_jacobi():
+    assert jacobi_symbol(29, 45) == 1
+    assert jacobi_symbol(30, 39) == 0
+    assert jacobi_symbol(28, 33) == -1
+
+def test_euler():
+    assert euler_phi(91) == 72
+    assert euler_phi(1) == 1
+    assert euler_phi(100) == 40
+    assert euler_phi(101) == 100
+
+def test_mobius():
+    assert mobius_mu(100) == 0
+    assert mobius_mu(10) == 1
+    assert mobius_mu(11) == -1
